c1f90c9660b32677658ce67ce8266a6ea00a9c38
shashikant010 <shashikantyadav0503@gmail.com>
2024-02-25 15:19:36 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const io = require('socket.io')(8000, {
+    cors: {
+      origin: "https://shashikant010.github.io",  // replace with your client's origin
+      methods: ["GET", "POST"],
+      allowedHeaders: ["my-custom-header"],
+      credentials: true
+    }
+  });
+  
+  const users={};
+  
+  io.on('connection',socket =>{
+      socket.on('new-user-joined',name=>{
+          console.log('new user ',name)
+          users[socket.id]=name;
+          socket.broadcast.emit('user-joined', name)
+      })
+      socket.on('send',message=>{
+          socket.broadcast.emit('receive',{message:message,name:users[socket.id]})
+      })
+      socket.on('disconnect',message=>{
+        socket.broadcast.emit('left',{name:users[socket.id],message:message});
+        delete users[socket.id];
+      })
+  })
+  
